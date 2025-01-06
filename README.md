98f52533920e6989634be9770e02f1d54db2030e
nmtuong09 <nmtuong09@gmail.com>
2025-01-06 16:26:37 +0700

    Edit all

--- README.md
+++
@@ -1,1 +0,0 @@
-# test_chi_tieu
--- main.py
+++ main.py
@@ -1,48 +1,57 @@
-
 import logging
-from datetime import datetime
+
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,CallbackContext
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 import requests
+from datetime import datetime
 
-
-# B1: Cấu hình Telegram bot trên telegram và lấy token để khai báo ở đây
-TELEGRAM_BOT_TOKEN ="7702193644:AAGopapZ-awNmcKrEMaVjX6HuilrJbXToP0"
-# B2: Cấu hình Webhook của google app scrip và khai báo tại đây
-WEEBHOOK_URL ="https://script.google.com/macros/s/AKfycbwPxksbBxn-GWocQO71UdXOBm3RJpHTPQ26CjzTh3LWmQNhX5X9d6c4Ms2efyHnzygz-A/exec"
+# Cấu hình Telegram bot
+TELEGRAM_BOT_TOKEN = "7702193644:AAGopapZ-awNmcKrEMaVjX6HuilrJbXToP0"
+WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbwPxksbBxn-GWocQO71UdXOBm3RJpHTPQ26CjzTh3LWmQNhX5X9d6c4Ms2efyHnzygz-A/exec"
 
 
 # Lệnh /start
-def start(update:Update, context: CallbackContext):
-    update.message.reply_text("Chào bạn! Gửi tin nhắn theo định dạng sau:\n\n<Nội dung> - <Số tiền>\n\nVD: Cà phê - 50000")
-# xử lý tin nhắn chi tiêu
-def xu_ly_tin(update: Update, context: CallbackContext):
+async def start(update: Update, context: CallbackContext):
+    await update.message.reply_text(
+        "Chào bạn! Gửi tin nhắn theo định dạng sau:\n\n<Nội dung> - <Số tiền>\n\nVD: Cà phê - 50000"
+    )
+
+# Xử lý tin nhắn chi tiêu
+async def xu_ly_tin(update: Update, context: CallbackContext):
     try:
+        # Lấy nội dung tin nhắn
         text = update.message.text
         content, amount = [x.strip() for x in text.split("-")]
 
+        # Lấy thời gian hiện tại
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        # gửi dữ liệu vào google apps scrip webhook
-        payload = {"date": current_time, "content": content,"amount": amount}
-        response = requests.post(WEEBHOOK_URL, json = payload)
+
+        # Gửi dữ liệu vào webhook
+        payload = {"date": current_time, "content": content, "amount": amount}
+        response = requests.post(WEBHOOK_URL, json=payload)
 
         if response.text == "Success":
-            update.message.reply_text("Ghi nhận chi tiêu thành công!")
+            await update.message.reply_text("Ghi nhận chi tiêu thành công!")
         else:
-            update.message.reply_text("Error: check lại dữ liệu!")
+            await update.message.reply_text("Error: kiểm tra lại dữ liệu!")
     except Exception as e:
         logging.error(e)
-        update.message.reply_text("Sai định dạng. Vui lòng gửi tin nhắn theo định dạng:\n<Nội dung> - <Số tiền>")
+        await update.message.reply_text(
+            "Sai định dạng. Vui lòng gửi tin nhắn theo định dạng:\n<Nội dung> - <Số tiền>"
+        )
+
 # Tạo bot Telegram
 def main():
-    update = Updater(TELEGRAM_BOT_TOKEN, use_context = True)
-    dispatcher = update.dispatcher
-    dispatcher.add_handler(CommandHandler("start",start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, xu_ly_tin))
+    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+    # application = application.sync_dispatcher
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, xu_ly_tin))
 
-    update.start_polling()
-    update.idle()
+    application.run_polling()
+
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
